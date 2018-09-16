6b4ac31907097d745488272618fa8b514d58309f
kitson <tkitson@udel.edu>
2018-09-15 22:11:46 -0400

    saving attempt at bluetooth device scanner

--- app/src/main/java/com/example/tkitson/roomrover/DeviceScanActivity.java
+++ app/src/main/java/com/example/tkitson/roomrover/DeviceScanActivity.java
@@ -1,7 +1,131 @@
 package com.example.tkitson.roomrover;
 
 import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Contacts;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
+import java.util.ArrayList;
+
+//based on example from:
+//https://developer.android.com/guide/topics/connectivity/bluetooth-le
 public class DeviceScanActivity extends ListActivity {
+    private ArrayList<BluetoothDevice> devices;
+    private BluetoothAdapter bluetoothAdapter;
+    private boolean isScanning;
+    private Handler handler;
 
+    private class DeviceScanAdapter implements ListAdapter {
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return false;
+        }
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return devices.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return devices.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return devices.isEmpty();
+        }
+    }
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        setContentView(R.layout.activity_device_scan);
+
+
+    }
+
+    private scanForLeDevice(final boolean enable){
+        BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
+            @Override
+            public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        deviceScanAdapter.addDevice(device);
+                        deviceScanAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }
+
+        if (enable) {
+            //set things up so that the we've stop scanning after a while
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    isScanning = false;
+                    bluetoothAdapter.stopLeScan(scanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            isScanning = true;
+            bluetoothAdapter.startLeScan(scanCallback)
+        } else {
+            isScanning = false;
+            bluetoothAdapter.stopLeScan(scanCallback);
+        }
+    }
 }
