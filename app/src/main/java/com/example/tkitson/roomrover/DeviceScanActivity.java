6230d14270032054618a2e00f3546a284e7182b7
kitson <tkitson@udel.edu>
2018-09-15 20:31:00 -0400

    actually can switch activities now

--- app/src/main/java/com/example/tkitson/roomrover/DeviceScanActivity.java
+++ app/src/main/java/com/example/tkitson/roomrover/DeviceScanActivity.java
@@ -3,4 +3,5 @@ package com.example.tkitson.roomrover;
 import android.app.ListActivity;
 
 public class DeviceScanActivity extends ListActivity {
+
 }
--- app/src/main/java/com/example/tkitson/roomrover/RoverMain.java
+++ app/src/main/java/com/example/tkitson/roomrover/RoverMain.java
@@ -1,7 +1,10 @@
 package com.example.tkitson.roomrover;
 
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +18,19 @@ public class RoverMain extends AppCompatActivity {
         TextView textView = findViewById(R.id.text);
         OnArrowClickListener textViewUpdater = new OnArrowClickListener(textView);
 
+        //setup the Bluetooth button
+        Button bluetooth_button = findViewById(R.id.bluetooth_button);
+        bluetooth_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //switch to the device scanning activity when the button is pressed
+                Intent deviceScanIntent =
+                        new Intent(RoverMain.this, DeviceScanActivity.class);
+                RoverMain.this.startActivity(deviceScanIntent);
+            }
+        });
+
+        //setup the arrow buttons
         Button left_arrow_button = findViewById(R.id.left_arrow_button);
         Button right_arrow_button = findViewById(R.id.right_arrow_button);
 
