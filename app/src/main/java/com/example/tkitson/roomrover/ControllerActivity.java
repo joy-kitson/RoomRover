765934b42937f37e55c995c8a70d7b9fbbb38f92
kitson <tkitson@udel.edu>
2018-09-16 04:19:41 -0400

    moved arrow buttons to controller activity

--- app/src/main/java/com/example/tkitson/roomrover/ControllerActivity.java
+++ app/src/main/java/com/example/tkitson/roomrover/ControllerActivity.java
@@ -3,6 +3,8 @@ package com.example.tkitson.roomrover;
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ControllerActivity extends Activity {
     private BluetoothDevice device;
@@ -10,8 +12,22 @@ public class ControllerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.controller_layout);
+
         device = (BluetoothDevice) getIntent()
                 .getParcelableExtra(BluetoothDevice.class.getName());
 
+        TextView textView = findViewById(R.id.text);
+        OnArrowClickListener textViewUpdater = new OnArrowClickListener(textView);
+
+        //setup the arrow buttons
+        int[] button_ids = {R.id.left_arrow_button,
+                R.id.right_arrow_button,
+                R.id.up_arrow_button,
+                R.id.down_arrow_button};
+        for (int i = 0; i < button_ids.length; i++){
+            Button button = findViewById(button_ids[i]);
+            button.setOnClickListener(textViewUpdater);
+        }
     }
 }
--- app/src/main/java/com/example/tkitson/roomrover/RoverMain.java
+++ app/src/main/java/com/example/tkitson/roomrover/RoverMain.java
@@ -18,9 +18,6 @@ public class RoverMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rover_main);
 
-        TextView textView = findViewById(R.id.text);
-        OnArrowClickListener textViewUpdater = new OnArrowClickListener(textView);
-
         //setup the Bluetooth button
         Button bluetooth_button = findViewById(R.id.bluetooth_button);
         bluetooth_button.setOnClickListener(new View.OnClickListener() {
@@ -35,17 +32,5 @@ public class RoverMain extends AppCompatActivity {
                 RoverMain.this.startActivity(deviceScanIntent);
             }
         });
-
-        /*
-        //setup the arrow buttons
-        int[] button_ids = {R.id.left_arrow_button,
-                            R.id.right_arrow_button,
-                            R.id.up_arrow_button,
-                            R.id.down_arrow_button};
-        for (int i = 0; i < button_ids.length; i++){
-            Button button = findViewById(button_ids[i]);
-            button.setOnClickListener(textViewUpdater);
-        }
-        */
     }
 }
