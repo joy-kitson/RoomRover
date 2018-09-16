4e695dd48004362afa483b67523b8c113a4b9f9c
kitson <tkitson@udel.edu>
2018-09-15 23:14:27 -0400

    device scaner activity actually opens again (with actual code written...even though nothing actually is visible...)

--- app/src/main/java/com/example/tkitson/roomrover/DeviceScanActivity.java
+++ app/src/main/java/com/example/tkitson/roomrover/DeviceScanActivity.java
@@ -4,17 +4,22 @@ import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Contacts;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 //based on example from:
 //https://developer.android.com/guide/topics/connectivity/bluetooth-le
@@ -23,93 +28,44 @@ public class DeviceScanActivity extends ListActivity {
     private BluetoothAdapter bluetoothAdapter;
     private boolean isScanning;
     private Handler handler;
+    private DeviceScanAdapter deviceScanAdapter;
 
-    private class DeviceScanAdapter implements ListAdapter {
+    private class DeviceScanAdapter extends ArrayAdapter<BluetoothDevice> {
 
-        @Override
-        public boolean areAllItemsEnabled() {
-            return false;
-        }
-
-        @Override
-        public boolean isEnabled(int position) {
-            return false;
-        }
-
-        @Override
-        public void registerDataSetObserver(DataSetObserver observer) {
-
-        }
-
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver observer) {
-
-        }
-
-        @Override
-        public int getCount() {
-            return devices.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return devices.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            return 0;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return devices.isEmpty();
+        public DeviceScanAdapter(@NonNull Context context, int resource) {
+            super(context, resource, devices);
         }
     }
+
 
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        setContentView(R.layout.activity_device_scan);
+        super.onCreate(savedInstanceState);
 
+        Toast toast = Toast.makeText(this, "creating DeviceScanActivity", Toast.LENGTH_SHORT);
+        toast.show();
+        setContentView(R.layout.activity_device_scan);
+        devices = new ArrayList<>();
+        deviceScanAdapter = new DeviceScanAdapter(this, android.R.layout.simple_list_item_1);
 
     }
 
-    private scanForLeDevice(final boolean enable){
-        BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
+    private void scanForLeDevice(final boolean enable){
+        final BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
             @Override
             public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        deviceScanAdapter.addDevice(device);
+                        deviceScanAdapter.add(device);
                         deviceScanAdapter.notifyDataSetChanged();
                     }
                 });
             }
-        }
+        };
 
         if (enable) {
             //set things up so that the we've stop scanning after a while
@@ -122,7 +78,7 @@ public class DeviceScanActivity extends ListActivity {
             }, SCAN_PERIOD);
 
             isScanning = true;
-            bluetoothAdapter.startLeScan(scanCallback)
+            bluetoothAdapter.startLeScan(scanCallback);
         } else {
             isScanning = false;
             bluetoothAdapter.stopLeScan(scanCallback);
--- app/src/main/java/com/example/tkitson/roomrover/RoverMain.java
+++ app/src/main/java/com/example/tkitson/roomrover/RoverMain.java
@@ -2,11 +2,14 @@ package com.example.tkitson.roomrover;
 
 import android.bluetooth.BluetoothManager;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RoverMain extends AppCompatActivity {
 
@@ -23,10 +26,22 @@ public class RoverMain extends AppCompatActivity {
         bluetooth_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast toast = Toast.makeText(RoverMain.this, "switching to DeviceScanActivity", Toast.LENGTH_SHORT);
+                toast.show();
+
                 //switch to the device scanning activity when the button is pressed
-                Intent deviceScanIntent =
-                        new Intent(RoverMain.this, DeviceScanActivity.class);
-                RoverMain.this.startActivity(deviceScanIntent);
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Toast toast = Toast.makeText(RoverMain.this, "switched to DeviceScanActivity", Toast.LENGTH_SHORT);
+                        //toast.show();
+                        Intent deviceScanIntent =
+                                new Intent(RoverMain.this, DeviceScanActivity.class);
+                        RoverMain.this.startActivity(deviceScanIntent);
+                    }
+                },2000);
             }
         });
 
